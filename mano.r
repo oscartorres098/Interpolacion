9b7503ec8775c8323e0df4aed9c2ba7c5eb5f7ec
oscartorres098 <36300388+oscartorres098@users.noreply.github.com>
2018-04-11 08:53:41 -0500

    Update mano.r

--- mano.r
+++ mano.r
@@ -3,19 +3,19 @@ x=c(14.6, 14.8, 15.2, 15.7, 17.0,  17.5, 17.3, 16.8, 15.4, 15.0, 14.4, 14.5, 15.
 y=c(14.7, 12.3, 11.0, 10.2, 8.20,  6.70, 6.60, 6.80, 8.30, 8.80, 9.10, 8.80, 6.30, 5.50, 5.00, 4.70, 4.60, 5.40, 5.80, 6.90, 8.20, 7.60, 5.2, 4.50, 4, 4.2, 5.70, 7.00, 7.80, 8.30, 7.30, 6.70, 5.50, 5.50, 4.60, 4.4, 5.5, 7.2, 7.8, 8.60, 9.40, 10.0, 10.7, 9.9, 9.25, 9.1, 9.3, 11.7, 12.3,  13.0, 13.7,  15.5)     
 
 
-plot(x,y, pch=19, cex=1, col = "red", asp=1,xlab="X", ylab="Y", main="Diagrama ")
+plot(x,-y, pch=19, cex=1, col = "red", asp=1,xlab="X", ylab="Y", main="Diagrama ")
 
 Interpolacion <- function(x,y) {
-
+  
   a = rep(y)
   n = length(x)
-
+  
   h <- (c(x,0) - c(0,x))[2:n]
   alph <- (3/c(1,h,1,1)*(c(a,1,1) - c(1,a,1)) - 3/c(1,1,h,1)*(c(1,a,1)-c(1,1,a)))[3:n]
   
   A <- c(1,rep(0,times=n-1))
   for (i in 1:(n-2)) {
-      A <- rbind(A,c( rep(0,times=i-1) , c(h[i],2*(h[i]+h[i+1]),h[i+1]) , rep(0,times=n-i-2) ) )
+    A <- rbind(A,c( rep(0,times=i-1) , c(h[i],2*(h[i]+h[i+1]),h[i+1]) , rep(0,times=n-i-2) ) )
   }
   
   A <- rbind(A,c(rep(0,times=n-1),1))
@@ -42,4 +42,4 @@ draw <- function(x,y) {
     points(dat$x,dat$y,type='l', col='red')
   }
 }
-draw(x,y)
+draw(x,-y)
